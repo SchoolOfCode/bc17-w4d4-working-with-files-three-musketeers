43e50399826a7921fdffa406933310e0086f3c05
Richard Lamb <lamby42@live.co.uk>
2024-07-25 13:36:36 +0100

    task 2b code written, fails tests. not sure if repo test is wrong?

--- quote.js
+++ quote.js
@@ -2,10 +2,19 @@ import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
-
+// take in no arguments
+//read/parse all quote objects from the quotes.json, defined as fileName
+//return an array of all existing quote objects
+//function will use fs.readFile to read the contents of the file
+//function will be asynchronous so will require try and catch block
 export async function getQuotes() {
-
-}
+    try {
+        const quotes = await fs.readFile(fileName, "utf8");
+        return JSON.parse(quotes);
+    } catch (error) {
+        console.error(`Error reading file: ${error}`);
+};
+};
 
 export async function addQuote(quoteText) {
 
