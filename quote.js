ee7196ebeeb80e78f964b6a9da4ebe2cdbaac19a
Richard Lamb <lamby42@live.co.uk>
2024-07-25 17:01:54 +0100

    ticket 2c code for addQuote added - passes test. 2b also passes now with 2c working. AddedTypeError handler on 2b, just for demo purposes. change to utf16 to see it log

--- quote.js
+++ quote.js
@@ -11,23 +11,53 @@ export async function getQuotes() {
     try {
         const quotes = await fs.readFile(fileName, "utf8");
         return JSON.parse(quotes);
+        //read docs for fs and errors
+    } catch (TypeError) {
+        console.log(TypeError + "oh no what a shame");
+    }
+}
+//function should take in a quoteText string
+//create a quote object
+//assign unique id with uuid to that object
+//read/parse all quote objects from the quotes.json, defined as fileName
+//save the newly generated quote object to the quotes.json file at the end of the array.
+//return the newly generated quote object
+
+//dummy - didn't see they want us to read the file again and then add stuff to it. - really inefficient, no?
+export async function addQuote(quoteText) {
+    try {
+        // retrieve all existing quotes using getQuotes
+        const existingQuotes = await getQuotes();
+        //create new quote object
+        const newQuote = {
+            id: uuidv4(),
+            quoteText,
+        };
+        // add new quote to the array of existing quotes - we can do this because we have read the file and have existing quotes in memory
+        existingQuotes.push(newQuote);
+        // write the ypdated existing quotes array (with new quote pushed on)
+        await fs.writeFile(fileName, JSON.stringify(existingQuotes));
+        //return the new quote object, not the whole array
+        return newQuote;
     } catch (error) {
         console.error(`Error reading file: ${error}`);
-};
-};
-
-export async function addQuote(quoteText) {
-
+    }
 }
+//     try {
+//         const newQuote = {
+//             id: uuidv4(),
+//             quoteText,
+//         };
+//         const addingQuotes = await fs.writeFile(fileName);
+//         return addingQuotes
+//     }
+//     catch (error) {
+//         console.error(`Error reading file: ${error}`);
+// }
+// };
 
-export async function getRandomQuote() {
+export async function getRandomQuote() {}
 
-}
+export async function editQuote(id, quoteText) {}
 
-export async function editQuote(id, quoteText) {
-
-}
-
-export async function deleteQuote(id) {
-
-}
+export async function deleteQuote(id) {}
